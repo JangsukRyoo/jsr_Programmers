36df22f038eed5ca5acb7f42db51bf8032b0dd43
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-10 22:33:00 +0900

    LEVEL 1  평균구하기

---
+++ src/lvl1/getAvg.java
@@ -0,0 +1,17 @@
+package lvl1;
+
+public class getAvg {
+    public static void main(String[] args){
+        int[] arr = new int[3];
+
+        double avg = 0;
+        double sum = 0;
+
+        for(int i= 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        avg = sum / arr.length;
+
+        System.out.println(avg);
+    }
+}
