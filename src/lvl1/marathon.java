f240b1a1c3d9bc7c858baa09cbedd381f9137375
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-16 22:22:40 +0900

    LEVEL 1 마라톤 완주 못한사람

---
+++ src/lvl1/marathon.java
@@ -0,0 +1,33 @@
+package lvl1;
+
+import java.util.HashMap;
+
+public class marathon {
+    public static void main(String args[]) {
+
+            //에제
+            String[] participant = {"leo", "kiki", "eden"};
+            String[] completion = {"leo", "kiki"};
+
+    //        String[] participate = {"mislav","stanko","mislav","ana","mislav"};
+    //        String[] completion = {"mislav","stanko","ana"};
+            String answer = "";
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for (String name : participant) {
+                map.put(name, map.getOrDefault(name, 0) + 1);
+            }
+
+            for (String name : completion) {
+                map.put(name, map.get(name) - 1);
+            }
+
+            for (String key : map.keySet()) {
+                if (map.get(key) > 0) {
+                    answer = key;
+                    break;
+                }
+            }
+        System.out.println(answer);
+    }
+}
