d81172a2e2a23cc78332cae09014c454b6915e89
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-17 23:46:22 +0900

    LEVEL 1 비밀지도 수정

--- src/lvl1/SecretMap.java
+++ src/lvl1/SecretMap.java
@@ -8,16 +8,14 @@ public class SecretMap {
         int[] arr1 ={9,20,28,18,11};
         int[] arr2={30,1,21,17,28};
 
-    for(int i =0; i < n; i++){
-        answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-        }
+         for(int i =0; i < n; i++){
+             answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+              }
 
-    for(int i = 0; i<n; i++){
-        answer[i] = String.format("%" + n + "s", answer[i]);
-        answer{i} = answer[i].replace("1","#");
-        answer[i] = answer[i].replace("0","");
-
-
-    }
+         for(int i = 0; i<n; i++){
+             answer[i] = String.format("%" + n + "s", answer[i]);
+             answer[i] = answer[i].replace("1","#");
+             answer[i] = answer[i].replace("0","");
+         }
     }
 }
