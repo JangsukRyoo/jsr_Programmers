72498885d0e78c11be3eacfd14053bfcc2345e4e
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-20 22:18:53 +0900

    LEVEL 1 예산 문제

---
+++ src/lvl1/Budget.java
@@ -0,0 +1,20 @@
+package lvl1;
+
+import java.util.Arrays;
+
+public class Budget {
+    public int answer(int[] d, int budget){
+        int sum = 0;
+        int ct = 0;
+        Arrays.sort(d);
+
+        for(int i = 0; i<d.length; i++){
+            if(sum + d[i] > budget){
+                break;
+            }
+            sum += d[i];
+            ct ++;
+        }
+        return ct;
+    }
+}
