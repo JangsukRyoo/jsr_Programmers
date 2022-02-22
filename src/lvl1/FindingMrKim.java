2b7d9f2f770127bfbd8ca4492908228c15fba0ad
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-22 22:53:08 +0900

    LEVEL 1 서울에서 김서방 찾기

---
+++ src/lvl1/FindingMrKim.java
@@ -0,0 +1,15 @@
+package lvl1;
+
+public class FindingMrKim {
+    public static void main(String args[]){
+        String [] seoul ={"Jane","Kim"};
+        String answer ="";
+
+        for(int i =0; i<seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer += "김서방은 " + i + "에 있다.";
+                System.out.println(answer);
+            }
+        }
+    }
+}
