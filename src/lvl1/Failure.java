a25dd2b775dcf3c64ba37520be0a3c20d5863c79
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-23 13:56:10 +0900

    LEVEL 1 실패율

---
+++ src/lvl1/Failure.java
@@ -0,0 +1,67 @@
+package lvl1;
+
+import javax.xml.ws.EndpointReference;
+import java.util.*;
+
+public class Failure {
+    public static void main (String args[]){
+        int [] answer = {};
+        int n = 5;
+        int [] stages = {2,1,2,6,2,4,3,3};
+
+        answer = solution(n,stages);
+
+    }
+
+    public static int[] solution(int N, int[] stages){
+        int[] answer = new int[N];
+        int length = stages.length;
+        int ct = 0;
+
+        //스테이지 정렬
+        Arrays.sort(stages);
+
+        // 중복 확인 (실패율 계산을 위한 중복 확인)
+        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer,Double> failure = new HashMap<>();
+
+        // 같은 수 정리
+        for(int stage : stages){
+            if(stage == N+1){
+                stage = stage -1;
+                map.put(stage,map.getOrDefault(stage,0)+1);
+            }else {
+                map.put(stage, map.getOrDefault(stage, 0) + 1);
+            }
+        }
+
+        //정리 및 계산을 위한 entry로 변경
+        List<Map.Entry<Integer,Integer>> entryList = new LinkedList<>(map.entrySet());
+
+        //실패율 계산
+        for(Map.Entry<Integer,Integer> entry : entryList){
+            double fail = 0;
+            double chk = ((double)entry.getValue()/(double)length);
+            if(chk == 1){
+                fail = 0;
+            }else{
+                fail = chk;
+            }
+
+            failure.put(entry.getKey(),fail);
+            System.out.println(fail);
+            length -= entry.getValue();
+        }
+
+        // 오름차수 정렬
+        List<Map.Entry<Integer,Double>> failEntryList = new LinkedList<>(failure.entrySet());
+        failEntryList.sort(Map.Entry.comparingByValue());
+
+        for(Map.Entry<Integer,Double> fEntry: failEntryList){
+            answer[ct] = fEntry.getKey();
+            ct ++;
+        }
+
+        return answer;
+    }
+}
