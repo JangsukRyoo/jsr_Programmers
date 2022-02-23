ccfeb0f06121ef6f9bc79061ae74a4ecdca40f5b
Jangsuk Ryoo <jangsukr@gmail.com>
2022-02-23 11:44:02 +0900

    LEVEL 1 가장 작은수 제거하기

---
+++ src/lvl1/EliminateSmallestNum.java
@@ -0,0 +1,35 @@
+package lvl1;
+
+public class EliminateSmallestNum {
+    public static void main(String args[]){
+        int[] arr = {4,3,2,1};
+        int[] answer = {};
+        answer = solution(arr);
+
+    }
+    static int[] solution(int[] arr) {
+        int[] ans = {};
+        int ansct =0; // 작은수가 중간에 있을 경우 런타임 에러가남 순서대로 증가하지 않음
+
+        // -1 처리
+        if (arr.length <= 1) {
+            return new int[]{-1};
+        }
+
+        // 작은수 걸러내기
+        int minIdx = 0;
+        for(int ct = 0; ct < arr.length; ct++){
+            if(arr[minIdx] > arr[ct]) {
+                minIdx = ct;
+            }
+        }
+
+        //작은수 배열 제거
+        for(int ct = 0; ct < arr.length; ct++){
+            if(ct != minIdx){
+                ans[ansct++] = arr[ct];
+            }
+        }
+        return ans;
+    }
+}
