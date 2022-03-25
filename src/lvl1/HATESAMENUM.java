7e3ee30383551b1a918e3a687b5aae5b6f573321
Jangsuk Ryoo <jangsukr@gmail.com>
2022-03-25 23:32:00 +0900

    level 1 같은숫자는 싫어

---
+++ src/lvl1/HATESAMENUM.java
@@ -0,0 +1,27 @@
+package lvl1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class HATESAMENUM {
+
+    public static void main(String args[]){
+        int[] arr = {1,1,3,3,0,1,1};
+        ArrayList<Integer> tmpArr = new ArrayList<>();
+        int n = 10;
+
+        for(int num : arr){
+            if(n != num){
+                tmpArr.add(num);
+                n = num;
+            }
+        }
+
+        int[] answer = new int[tmpArr.size()];
+        for(int i = 0; i<answer.length; i++){
+            answer[i] = tmpArr.get(i).intValue();
+        }
+    }
+
+}
