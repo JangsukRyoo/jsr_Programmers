a990a4e396704586914635236c8157266cd831ed
Jangsuk Ryoo <jangsukr@gmail.com>
2022-03-26 23:41:28 +0900

    level 1 parseInt

---
+++ src/lvl1/ParseINT.java
@@ -0,0 +1,14 @@
+package lvl1;
+
+public class ParseINT {
+    public static void main(String args[]){
+        String s = "123";
+        String s1 = "-123";
+
+        int a = Integer.parseInt(s);
+        int a1 =Integer.parseInt(s1);
+
+        System.out.println(a);
+        System.out.println(a1);
+    }
+}
