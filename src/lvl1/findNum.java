0345bffe331df2449154ace3f4759295e6a94095
Jangsuk Ryoo <jangsukr@gmail.com>
2022-03-29 23:53:30 +0900

    level 1 find sqrt

---
+++ src/lvl1/findNum.java
@@ -0,0 +1,24 @@
+package lvl1;
+
+public class findNum {
+    public static void main(String args[]){
+        int n = 10;
+        int answer = 0;
+
+        for(int i = 2; i<=n; i++){
+            boolean fl = true;
+
+            for(int j=2; j<Math.sqrt(i); j++){
+                if(i%j == 0){
+                    fl= false;
+                    break;
+                }
+            }
+
+            if(fl == true)
+                answer ++;
+
+        }
+        System.out.println(answer);
+    }
+}
