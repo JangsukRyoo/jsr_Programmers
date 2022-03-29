79e70b164033ea6d0275c30e600a4fd48abf2285
Jangsuk Ryoo <jangsukr@gmail.com>
2022-03-30 00:26:28 +0900

    level 1 ReverseNum

---
+++ src/lvl1/FlipNUM.java
@@ -0,0 +1,20 @@
+package lvl1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FlipNUM {
+    public static void main(String args[]){
+        int n = 118372;
+        String s = String.valueOf(n);
+        long answer =0;
+
+        String[] numArr = s.split("");
+
+        Arrays.sort(numArr);
+        Collections.reverse(Arrays.asList(numArr));
+        answer = Long.parseLong(String.join("",numArr));
+
+        System.out.println(answer);
+    }
+}
