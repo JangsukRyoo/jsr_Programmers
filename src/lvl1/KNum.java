08c0abf17ee07b199c59a9c017a0bb69077c7cf3
Jangsuk Ryoo <jangsukr@gmail.com>
2022-04-03 23:22:11 +0900

    level 1  Knum

---
+++ src/lvl1/KNum.java
@@ -0,0 +1,17 @@
+package lvl1;
+
+public class KNum {
+    public static void main(String args[]){
+
+    int[] array = {1,5,2,6,3,7,4};
+    int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+
+    int[] answer = new int[commands.length];
+
+
+
+
+
+
+    }
+}
