d2d658965b5e00986d59b60fd9855816eaa1c4c1
Jangsuk Ryoo <jangsukr@gmail.com>
2022-04-08 23:36:28 +0900

    Level 2 country 124

---
+++ src/lvl2/Country124.java
@@ -0,0 +1,15 @@
+package lvl2;
+
+public class Country124 {
+    public static void main(String args[]){
+    int n = 10;
+    String answer ="";
+    String[] num ={"4","1","2"};
+
+    while(n >0){
+        answer = num[n%3]+answer;
+        n = (n -1)/3;
+    }
+        System.out.println(answer);
+    }
+}
