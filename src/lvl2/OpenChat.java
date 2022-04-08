f273331ee54bca7a2cbc01e18612d06686ce913c
Jangsuk Ryoo <jangsukr@gmail.com>
2022-04-09 00:04:26 +0900

    Level 2 OpenChat 수정

--- src/lvl2/OpenChat.java
+++ src/lvl2/OpenChat.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 public class OpenChat {
     public static void main(String args[]){
         String [] record ={"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
-        String[] answer ={};
         HashMap<String, String> users = new HashMap<String, String>();
 
         StringBuilder sb = new StringBuilder();
@@ -19,7 +18,6 @@ public class OpenChat {
                     break;
                 case "Leave":
                     sb.append((arr[1])).append("님이 나갔습니다.-");
-                    users.put(arr[1], arr[2]);
                     break;
                 case "Change":
                     users.put(arr[1], arr[2]);
@@ -28,14 +26,14 @@ public class OpenChat {
         }
 
             String str = sb.toString();
-            String[] result = str.split("-");
+            String[] answer = str.split("-");
 
-            for(int i = 0; i < result.length; i++) {
-                String key = result[i].substring(0, result[i].indexOf("님"));
-                result[i] = result[i].replace(key, users.get(key));
+            for(int i = 0; i < answer.length; i++) {
+                String key = answer[i].substring(0, answer[i].indexOf("님"));
+                answer[i] = answer[i].replace(key, users.get(key));
             }
 
-        System.out.println(result);
+        System.out.println(answer);
 
     }
 }
