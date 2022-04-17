7af30d7c29a1fb689e1c289bcec2308c9f50823b
Jangsuk Ryoo <jangsukr@gmail.com>
2022-04-17 23:42:10 +0900

    Level 2 PhoneBook

---
+++ src/lvl2/PhoneBook.java
@@ -0,0 +1,24 @@
+package lvl2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneBook {
+    public static void main(String args[]){
+    String[] phone_book = {"119", "97674223", "1195524421"};
+    Map<String, Integer> map = new HashMap<>();
+    boolean answer= true;
+    for(int i = 0; i <phone_book.length; i++){
+        map.put(phone_book[i], i);
+    }
+
+    for(int i =0; i< phone_book.length; i++){
+        for(int j =0; j<phone_book[i].length(); j++){
+            if(map.containsKey(phone_book[i].substring(0,j))){
+                answer= false;
+            }
+        }
+    }
+        System.out.println(answer);
+    }
+}
