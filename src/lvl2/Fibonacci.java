ba0e5a999c40266af05068f354dc6e577e18f373
Jangsuk Ryoo <jangsukr@gmail.com>
2022-04-19 22:19:12 +0900

    Level 2 fibonacci

---
+++ src/lvl2/Fibonacci.java
@@ -0,0 +1,22 @@
+package lvl2;
+
+public class Fibonacci {
+    public static void main (String args[]){
+        int answer = 0;
+        int n = 3;
+
+        int num1 = 1;
+        int num2 = 1;
+
+        if(n==1 || n==2) {
+            System.out.println(1);
+        }else{
+            for(int i = 3; i<=n; i++){
+                answer = (num1+num2)%1234567;
+                num1 = num2;
+                num2 = answer;
+            }
+            System.out.println(answer);
+        }
+    }
+}
