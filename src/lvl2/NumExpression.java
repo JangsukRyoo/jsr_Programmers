cf960d4889f9a1a8fbca4853c67467c4e228ea4b
Jangsuk Ryoo <jangsukr@gmail.com>
2022-05-01 15:47:10 +0900

    Level 2 NumExpression

---
+++ src/lvl2/NumExpression.java
@@ -0,0 +1,27 @@
+package lvl2;
+
+public class NumExpression {
+    public static void main(String args[]){
+
+        int num = 15;
+        int answer = 0;
+        int sum = 0;
+        for(int i = 1; i<=num; i++){
+                sum = i;
+                if(sum == num){
+                    solution++;
+                }
+            for(int n = 1; n<num-i; n++) {
+                sum += n;
+                if (sum == num) {
+                    solution++;
+                    sum = 0;
+                } else if (sum >num) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(solution);
+    }
+}
---
+++ src/lvl2/RenewMenu.java
@@ -0,0 +1,7 @@
+package lvl2;
+
+public class RenewMenu {
+    public static void main(String args[]){
+
+    }
+}
