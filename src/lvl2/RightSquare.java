21409e333bba1bd53c9be1d090bb768702c669a8
Jangsuk Ryoo <jangsukr@gmail.com>
2022-05-29 23:00:21 +0900

    Level 2 멀쩡한사각형

---
+++ src/lvl2/RightSquare.java
@@ -0,0 +1,14 @@
+package lvl2;
+
+public class RightSquare {
+    public static void main(String args[]){
+        long w = 8;
+        long h = 12;
+        long answer = 0;
+
+        for(int i=0; i<w; i++){
+            answer += h*i/w;
+        }
+        System.out.println(answer*2);
+    }
+}
