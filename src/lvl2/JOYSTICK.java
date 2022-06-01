bc64db002722d6294446b5e38130f97b061b24ed
Jangsuk Ryoo <jangsukr@gmail.com>
2022-06-02 00:02:46 +0900

    Level 2 조이스틱 문제 틀려 수정

--- src/lvl2/JOYSTICK.java
+++ src/lvl2/JOYSTICK.java
@@ -5,28 +5,46 @@ public class JOYSTICK {
     public static void main(String args[]){
         String name = "JAN";
         int answer = 0;
-        int len = name.length();
+//        int len = name.length();
+//
+//        int move = len-1;
+//
+//        for(int i=0; i<len; i++){
+//            //상하 이동
+//            if(name.charAt(i)> 'N'){
+//                answer += 'Z' - name.charAt(i) +1;
+//            }
+//            //'A'~'N'의 경우
+//            else{
+//                answer += name.charAt(i) - 'A';
+//            }
+//            int conA = i+1;
+//            //되돌아가는것이 빠른지 확인
+//            while(conA < len && name.charAt(conA) == 'A'){
+//                conA++;
+//            }
+//            move = Math.min(move, i+len-conA+i);
+//
+//        }
+//        answer+=move;
 
-        int move = len-1;
+        //코드 수정
+        int length = name.length();
 
-        for(int i=0; i<len; i++){
-            //상하 이동
-            if(name.charAt(i)> 'N'){
-                answer += 'Z' - name.charAt(i) +1;
-            }
-            //'A'~'N'의 경우
-            else{
-                answer += name.charAt(i) - 'A';
-            }
-            int conA = i+1;
-            //되돌아가는것이 빠른지 확인
-            while(conA < len && name.charAt(conA) == 'A'){
-                conA++;
-            }
-            move = Math.min(move, i+len-conA+i);
+        int index;
+        int move = length - 1;
 
+        for(int i = 0; i < name.length(); i++){
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+
+            index = i + 1;
+
+            while(index < length && name.charAt(index) == 'A'){
+                index++;
+            }
+            move = Math.min(move, i * 2 + length - index);
+            move = Math.min(move, (length - index) * 2 + i);
         }
-        answer+=move;
-        System.out.println(answer);
+        System.out.println(answer+move);
     }
 }
