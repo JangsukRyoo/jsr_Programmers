1be2bc914a238565ee93d11eb501f8ab4cc3d8f8
Jangsuk Ryoo <jangsukr@gmail.com>
2022-06-28 22:21:14 +0900

    Level 2 해쉬함수 만들기

---
+++ src/RandomPractice/MakeHash.java
@@ -0,0 +1,16 @@
+package RandomPractice;
+
+public class MakeHash {
+    public static void main(String args[]) {
+        int[] a = {1,2,3,4,5};
+
+        int num =  34;
+
+
+        int answer = num * 23;
+
+        System.out.println(answer);
+
+
+    }
+}
