932e0e35e9f323544c87b0c8ca7fe00b0a768fc0
Jangsuk Ryoo <jangsukr@gmail.com>
2022-07-10 23:59:12 +0900

    Update Collatz.java

--- src/lvl1/Collatz.java
+++ src/lvl1/Collatz.java
@@ -6,25 +6,20 @@ public class Collatz {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         long num = sc.nextLong();
-
         int answer = 0;
-
         while (num != 1) {
-
             if (num % 2 == 0) {
                 num /= 2;
             } else {
                 num *= 3;
                 num += 1;
             }
-
             answer++;
             if (answer == 500) {
                 answer = -1;
                 break;
             }
         }
-
         System.out.println(answer);
     }
 }
