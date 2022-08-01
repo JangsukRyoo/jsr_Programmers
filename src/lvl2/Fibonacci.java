5c989295ec8108a44c3c03cf4e881f84b0db80e1
Jangsuk Ryoo <jangsukr@gmail.com>
2022-08-01 21:39:24 +0900

    Update Fibonacci.java

--- src/lvl2/Fibonacci.java
+++ src/lvl2/Fibonacci.java
@@ -4,10 +4,8 @@ public class Fibonacci {
     public static void main (String args[]){
         int answer = 0;
         int n = 3;
-
         int num1 = 1;
         int num2 = 1;
-
         if(n==1 || n==2) {
             System.out.println(1);
         }else{
